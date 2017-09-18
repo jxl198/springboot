aa3cdde1be5c0418887b87380ba0224c876ed0ab
jiangxl <jxl198@126.com>
2017-09-18 20:25:50 +0800

    new project

---
+++ src/main/java/com/jiangxl/springboot/HelloController.java
@@ -0,0 +1,15 @@
+package com.jiangxl.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "helloWorld";
+	}
+
+}
